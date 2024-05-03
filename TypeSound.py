36d0c5f84b345245f9b859c920976859c2b98c3b
dneifiend <dneifiend@gmail.com>
2024-05-04 00:51:27 +0900

    enter, tab, space sound.

--- TypeSound.py
+++ TypeSound.py
@@ -5,7 +5,6 @@ os.system("cls")
 
 print("[TypeSound v1.0]")
 print("키보드 입력 시 사운드가 출력됩니다.")
-print("\n")
 print("이 창을 닫거나, 활성화 한 상태로 Crtl+C 를 입력하면 종료됩니다.")
 
 
@@ -17,7 +16,19 @@ def play_music(file_path):
 
 while True:
     event = keyboard.read_event()
-    if event.event_type == keyboard.KEY_DOWN:
-        play_music("down.mp3")
-    if event.event_type == keyboard.KEY_UP:
-        play_music("up.mp3")
+    if event.name=="enter" or event.name=="tab":
+        if event.event_type == keyboard.KEY_DOWN:
+            play_music("special_down.mp3")
+        if event.event_type == keyboard.KEY_UP:
+            play_music("special_up.mp3")
+
+    elif event.name=="space":
+        if event.event_type == keyboard.KEY_DOWN:
+            play_music("special_down2.mp3")
+        if event.event_type == keyboard.KEY_UP:
+            play_music("special_up2.mp3")
+    else:
+        if event.event_type == keyboard.KEY_DOWN:
+            play_music("down.mp3")
+        if event.event_type == keyboard.KEY_UP:
+            play_music("up.mp3")
